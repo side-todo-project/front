2818e38dc1b558e4f01aaa2b3db10c6f9fa3f1c7
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-03-09 21:24:58 +0900

    #4 Feat-[token 쿠키에 저장]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,34 @@
 import { useUserInfo } from '@/components/common/userProvider';
 import TestBox from '@/components/TestBox';
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
-  const {user} = useUserInfo();
-  console.log('####', user);
+export default function Home({cookies}) {
+  const { user } = useUserInfo();
+  console.log('####', user, cookies);
   return (
     <div>
       <TestBox />
-      <ul>
-        <li>id: {user.id}</li>
-        <li>email: {user.email}</li>
-        <li>nickname: {user.nickname}</li>
-        <li>newUser: {user.newUser}</li>
-      </ul>
+      <h1>Logined: {Boolean(user).toString()}</h1>
+      {user && (
+        <ul>
+          <li>id: {user.id}</li>
+          <li>email: {user.email}</li>
+          <li>nickname: {user.nickname}</li>
+          <li>newUser: {user.newUser}</li>
+        </ul>
+      )}
+      <p>accessToken: {cookies?.accessToken}</p>
+      <p>refreshToken: {cookies?.refreshToken}</p>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { cookies } = context.req;
+
+  return {
+    props: {
+      cookies,
+    },
+  };
+};
