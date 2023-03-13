60e1c8a4aa024b6e1cfa255f27e5067ee699d24a
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-03-13 21:37:06 +0900

    #4 Fix-[localhost->배포 주소로 변경]

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-const BASEURL = 'http://localhost:3000';
+const BASEURL = 'http://3.34.73.43:3000';
 
 const Login = () => {
   return (
