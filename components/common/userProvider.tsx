e1ff362602914992c93d9882ebed10532ab3746d
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-03-16 21:35:52 +0900

    #4 Feat-[닉네임 설정 api 호출테스트 + 인터페이스추가]

--- components/common/userProvider.tsx
+++ components/common/userProvider.tsx
@@ -1,17 +1,21 @@
 // context api
+import { IUserInfo } from '@/types';
 import React, { createContext, useContext, useState } from 'react';
 
-export const UserContext = createContext({
+export const UserContext = createContext<{
+  user: IUserInfo | null;
+  setUser: React.Dispatch<React.SetStateAction<IUserInfo | null>>;
+}>({
   user: null,
   setUser: null,
 });
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<IUserInfo | null>(null);
 
   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 };
 
 export const useUserInfo = () => {
   return useContext(UserContext);
-}
+};
--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,11 @@
-export { }
+export interface IUserInfo {
+  email: string;
+  id: string;
+  newUser: boolean;
+  nickname: string;
+}
+
+export interface ITokens {
+  accessToken: string;
+  refreshToken: string;
+}
