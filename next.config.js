8b85b5808981335593773bd39107bb94df4d0a5c
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-03-23 20:14:18 +0900

    #9 Fix-[add auth api proxy]

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: 'http://3.34.73.43:3000/api/:path*',
+      },
+      {
+        source: '/auth/:path*',
+        destination: 'http://3.34.73.43:3000/auth/:path*',
       }
     ]
   }
