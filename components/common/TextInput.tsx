b62d025f67a35362f662fd4ab4b6bcf29d6c8dbb
vloc-liam-koh <48155178+imnotpizza@users.noreply.github.com>
2023-03-27 10:29:16 +0900

    #13 Feat-[eslint 관련 오류 수정]

--- components/common/TextInput.tsx
+++ components/common/TextInput.tsx
@@ -1,11 +1,5 @@
 import Palette from '@/styles/Palette';
-import React from 'react';
 import styled from 'styled-components';
-
-const Container = styled.div`
-  height: 60px;
-  max-width: 300px;
-`;
 
 const Input = styled.input.attrs({ type: 'text' })`
   width: 100%;
@@ -17,20 +11,6 @@ const Input = styled.input.attrs({ type: 'text' })`
   padding-left: 30px;
 `;
 
-const ErrorText = styled.p`
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 16px;
-  color: ${(p) => p.theme.Error};
-`;
-
-const TextInput = ({ isError, errorText, ...props }) => {
-  return (
-    <Container>
-      <Input {...props} />
-      {isError && <ErrorText>{errorText}</ErrorText>}
-    </Container>
-  );
-};
+const TextInput = ({ ...props }) => <Input {...props} />;
 
 export default TextInput;
--- components/common/TextInputWithError.tsx
+++ components/common/TextInputWithError.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import TextInput from './TextInput';
 
+interface IProps extends React.ButtonHTMLAttributes<HTMLInputElement> {
+  isError: boolean;
+  errorText: string;
+}
+
 const Container = styled.div`
   height: 60px;
 `;
@@ -13,7 +18,7 @@ const ErrorText = styled.p`
   color: ${(p) => p.theme.Error};
 `;
 
-const TextInputWithError = ({ isError, errorText, ...props }) => {
+const TextInputWithError = ({ isError, errorText, ...props }: IProps) => {
   return (
     <Container>
       <TextInput {...props} />
