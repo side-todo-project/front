0ecfa33be15286351fbf213832f980fb2a5b1e32
imnotpizza <48155178+imnotpizza@users.noreply.github.com>
2023-03-28 15:26:35 +0900

    #9 Feat-[schedule api 추가]

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,38 @@
+import API from "./API";
 
 
 export const setNickname = (client) => { 
   const nickname = client.nickname;
   const nicknameEl = document.querySelector('.nickname');
   nicknameEl.textContent = nickname;
+}
+
+
+
+
+
+// schedule
+// 일정 생성하기
+export const createSchedule = (schedule) => {
+  return API.post('/schedule', schedule);
+};
+
+// 일정 가져오기
+export const fetchSchedule = (scheduleId) => {
+  return API.get(`/schedule/${scheduleId}`);
+}
+
+// 일정 수정하기
+export const updateSchedule = (scheduleId, newSchedule) => {
+  return API.put(`/schedule/${scheduleId}`, newSchedule);
+}
+
+// 완료된 일정 체크하기
+export const checkSchedule = (scheduleId, index) => {
+  return API.put(`/schedule/check/${scheduleId}`, { scheduleIdx: index });
+}
+
+// 공개된 최신 일정불러오기
+export const fetchLatestSchedule = () => {
+  return API.get('/schedule/latest');
 }
