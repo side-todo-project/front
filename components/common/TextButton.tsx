50ca9798007488be3c0bb121b3383e95c9306d23
imnotpizza <48155178+imnotpizza@users.noreply.github.com>
2023-03-28 13:28:20 +0900

    #9 Feat-[Textbutton storybook 추가]

---
+++ components/common/TextButton.tsx
@@ -0,0 +1,17 @@
+import Palette from '@/styles/Palette';
+import Typo from '@/styles/Typo';
+import styled from 'styled-components';
+import Button from './Button';
+
+const TextButton = styled(Button)`
+  background: transparent;
+  border: none;
+  color: ${Palette.Neutral.N400};
+
+  &:hover {
+    background: transparent;
+    color: ${Palette.Primary.P200};
+  }
+`;
+
+export default TextButton;
